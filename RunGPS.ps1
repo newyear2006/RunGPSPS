5e23e5de69474718ba2d9370d029d97f60e60132
newyear2006 <newyear2006github@easysoftware.de>
2023-02-06 14:43:03 +0100

    Update RunGPS.ps1

--- RunGPS.ps1
+++ RunGPS.ps1
@@ -145,3 +145,63 @@ Function SaveTrainingGPSData {
 
 # zum Prüfen, was auf der anderen Seite ankommt: https://pipedream.com/requestbin 
 # früher: http://requestb.in/
+
+####### Benutzer und Passwort müssen hinterlegt werden!!
+$user='Benutzer'
+$password='Passwort'
+#######
+$uri = "https://www.rungps.net/login.jsp"
+
+$l=Invoke-WebRequest -Uri $uri -SessionVariable runGPS
+$l.Forms[0].Fields["userName"]=$user
+$l.Forms[0].Fields["password1"]=$password
+$l.Forms[0].Fields
+$r=Invoke-WebRequest -Uri ('https://www.rungps.net/' + $l.Forms[0].Action) -Method Post -Body $l -WebSession $runGPS
+#$runGPS.Cookies.GetCookies($uri)
+# Dieses Image muss aufgerufen werden, damit die passenden Cookies für den Domainwechsel vorhanden sind!
+# Der Domainwechsel findet zwischen rungps.net und gps-sport.net statt!
+$wp=Invoke-WebRequest -WebSession $runGPS -Uri $r.ParsedHtml.images[0].href 
+
+# Datumsangaben im ISO-Format YYYY-MM-DDD, gibt maximal 1000 Einträge zurück, evtl. muss der Datumsbereich durch zwei
+# oder mehrere Aufrufe gesplittet werden!
+$r2=Invoke-WebRequest -WebSession $runGPS -Uri "http://www.gps-sport.net/userRoutes.jsp?userName=$($User)&startDate=2006-10-18&endDate=2023-02-06&sport=&searchTerm=&submitButton=Aktualisieren#" -ContentType "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+
+$r3=Invoke-WebRequest -WebSession $runGPS -Uri "http://www.gps-sport.net/userTrainings.jsp?userName=$($user)&startDate=2006-10-26&endDate=2017-12-31&sport=&submitButton=Aktualisieren#" -ContentType "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+
+$r4=Invoke-WebRequest -WebSession $runGPS -Uri "http://www.gps-sport.net/userTrainings.jsp?userName=$($user)&startDate=2018-01-01&endDate=2023-02-06&sport=&submitButton=Aktualisieren#" -ContentType "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+
+### ROUTEN
+# Routes
+$htmlRoutes=$r2.ParsedHtml.body.childNodes[1].childNodes[0].childNodes[13].childNodes[1].childnodes
+# Anzahl
+$htmlRoutes.length
+$routes=$htmlRoutes | % {$r=@()} {$r+=NewRoute $_} {$r}
+
+# erste Route zum Testen speichern
+SaveRouteGPSData -ID $routes[0].ID -FileType GPX -Path C:\Temp\RunGPS
+
+# alle ermittelten Routen speichern
+$routes | % {SaveRoutesData -ID $_.ID -Path C:\Temp\RunGPS }
+
+### TRAININGS
+# Trainings
+$htmlTrainings=$r3.ParsedHtml.body.childNodes[2].childNodes[0].childNodes[1].childNodes
+# Anzahl
+$htmlTrainings.length
+$trainings=$htmlTrainings | % {$t=@()} {$t+=NewTraining $_} {$t}
+
+# ein Training zum Testen speichern
+SaveTrainingGPSData -ID $trainings[0].ID -FileType GPX -Path C:\Temp\RunGPS
+
+# alle Trainings von der ersten Abfrage speichern
+$trainings | % {SaveTrainingsData -ID $_.ID -Path C:\Temp\RunGPS }
+
+# Trainings
+$htmlTrainings=$r4.ParsedHtml.body.childNodes[2].childNodes[0].childNodes[1].childNodes
+# Anzahl
+$htmlTrainings.length
+$trainings=$htmlTrainings | % {$t=@()} {$t+=NewTraining $_} {$t}
+
+# alle Trainings von der zweiten Abfrage speichern
+$trainings | % {SaveTrainingsData -ID $_.ID -Path C:\Temp\RunGPS }
+
